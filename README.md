ce25ad2a148e6e3508881fa8f63f740217dc9837
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-30 11:21:37 -0400

    Version 0.2.0
    * uses class instead of functions, to make stuff easier
    * update readme

--- README.md
+++ README.md
@@ -1,33 +1,24 @@
 # @issuekit/core.js
 ## Usage:
+### Setup:
+```javascript
+const { Issuekit } = require('@issuekit/core')
+const issuekit =  new Issuekit({
+  auth: '0000000000000000000000000000000000000000',
+  repo: 'issuekit/core.js',
+  issue: '42'
+})
+```
 ### Comment on an issue:
 ```javascript
-const issuekit = require('@issuekit/core')
-issuekit.comment.add({
-  token: '0000000000000000000000000000000000000000', // Add a Personal Access Token here. It should at least have the public_repo scope.
-  body: 'This comment was made with [issuekit](https://github.com/issuekit/core.js)!', // The body of your comment. Supports markdown. Newlines are \n
-  issue: '42', // The issue number you want to comment on.
-  repo: 'issuekit/core.js' // The repository that has the issue
-})
+issuekit.addComment('Hello World')
 ```
 ### Add labels to an issue
 ```javascript
-const issuekit = require('@issuekit/core')
-issuekit.labels.add({
-  token: '0000000000000000000000000000000000000000', // Add a Personal Access Token here. It should at least have the public_repo scope.
-  labels: ['issuekit', 'bot'], // The labels you want as an array.
-  issue: '42', // The issue number you want to add labels to.
-  repo: 'issuekit/core.js' // The repository that has the issue.
-})
+issuekit.addLabels(['hello', 'world'])
 ```
 
 ### Add Assignees to an issue
 ```javascript
-const issuekit = require('@issuekit/core')
-issuekit.assignees.add({
-  token: '0000000000000000000000000000000000000000', // Add a Personal Access Token here. It should at least have the public_repo scope.
-  assignees: ['issuekit', 'nthnchu'], // The people you want to assign as an array.
-  issue: '42', // The issue number you want to add labels to.
-  repo: 'issuekit/core.js' // The repository that has the issue.
-})
+issuekit.addAssignees(['issuekit', 'nthnchu'])
 ```
--- index.js
+++ index.js
@@ -1,153 +1,68 @@
 const https = require('https')
-const commentCreate = (object) => {
-  const token = object.token
-  const body = object.body
-  const issue = object.issue
-  const repo = object.repo
-  if (!token) {
-    throw new Error('You must provide a token')
-  }
-  if (!body) {
-    throw new Error('You must provide a body')
-  }
-  if (!issue) {
-    throw new Error('You must provide an issue number')
-  }
-  if (!repo) {
-    throw new Error('You must provide a repository')
-  }
-  const data = JSON.stringify({
-    body: body
-  })
-  const req = https.request(
-    {
-      hostname: 'api.github.com',
-      port: 443,
-      path: `/repos/${repo}/issues/${issue}/comments`,
-      method: 'POST',
-      headers: {
-        'User-Agent': 'issuekit',
-        'Content-Type': 'application/json',
-        'Content-Length': data.length,
-        Authorization: 'token ' + token
-      }
-    },
-    (res) => {
-      if (res.statusCode !== 201) {
-        console.error('Status Code is ' + res.statusCode.toString())
-        res.on('data', (d) => {
-          process.stdout.write(d)
-        })
-      }
+exports.Issuekit = class Issuekit {
+  constructor ({ auth: token, repo, issue: number }) {
+    this.token = token
+    this.repo = repo
+    this.number = number
+    if (!token) {
+      throw new Error('You must provide a personal access token')
     }
-  )
-  req.on('error', (error) => {
-    throw error
-  })
-  req.write(data)
-  req.end()
-}
-
-const labelsAdd = (object) => {
-  const token = object.token
-  const labels = object.labels
-  const issue = object.issue
-  const repo = object.repo
-  if (!token) {
-    throw new Error('You must provide a token')
-  }
-  if (!labels) {
-    throw new Error('You must provide labels')
-  }
-  if (!issue) {
-    throw new Error('You must provide an issue number')
-  }
-  if (!repo) {
-    throw new Error('You must provide a repository')
-  }
-  const data = JSON.stringify({
-    labels: labels
-  })
-  const req = https.request(
-    {
-      hostname: 'api.github.com',
-      port: 443,
-      path: `/repos/${repo}/issues/${issue}/labels`,
-      method: 'POST',
-      headers: {
-        'User-Agent': 'issuekit',
-        'Content-Type': 'application/json',
-        'Content-Length': data.length,
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: 'token ' + token
-      }
-    },
-    (res) => {
-      if (res.statusCode !== 200) {
-        console.error('Status Code is ' + res.statusCode.toString())
-        res.on('data', (d) => {
-          process.stdout.write(d)
-        })
-      }
+    if (!repo) {
+      throw new Error('You must provide a repository')
     }
-  )
-  req.on('error', (error) => {
-    throw error
-  })
-  req.write(data)
-  req.end()
-}
-
-const assigneesAdd = (object) => {
-  const token = object.token
-  const assignees = object.assignees
-  const issue = object.issue
-  const repo = object.repo
-  if (!token) {
-    throw new Error('You must provide a token')
-  }
-  if (!assignees) {
-    throw new Error('You must provide assignees')
-  }
-  if (!issue) {
-    throw new Error('You must provide an issue number')
-  }
-  if (!repo) {
-    throw new Error('You must provide a repository')
-  }
-  const data = JSON.stringify({
-    assignees: assignees
-  })
-  const req = https.request(
-    {
-      hostname: 'api.github.com',
-      port: 443,
-      path: `/repos/${repo}/issues/${issue}/assignees`,
-      method: 'POST',
-      headers: {
-        'User-Agent': 'issuekit',
-        'Content-Type': 'application/json',
-        'Content-Length': data.length,
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: 'token ' + token
-      }
-    },
-    (res) => {
-      if (res.statusCode !== 201) {
-        console.error('Status Code is ' + res.statusCode.toString())
-        res.on('data', (d) => {
-          process.stdout.write(d)
-        })
-      }
+    if (!number) {
+      throw new Error('You must provide an issue number')
     }
-  )
-  req.on('error', (error) => {
-    throw error
-  })
-  req.write(data)
-  req.end()
-}
+  }
 
-exports.comment = { add: commentCreate }
-exports.labels = { add: labelsAdd }
-exports.assignees = { add: assigneesAdd }
+  httpsApiRequest (data, path) {
+    const req = https.request(
+      {
+        hostname: 'api.github.com',
+        port: 443,
+        path: `/repos/${this.repo}/issues/${this.number}/${path}`,
+        method: 'POST',
+        headers: {
+          'User-Agent': 'issuekit',
+          'Content-Type': 'application/json',
+          'Content-Length': data.length,
+          Authorization: 'token ' + this.token
+        }
+      },
+      (res) => {
+        if (res.statusCode !== 201) {
+          console.error('Status Code is ' + res.statusCode.toString())
+          res.on('data', (d) => {
+            process.stderr.write(d)
+          })
+        }
+      }
+    )
+    req.on('error', (error) => {
+      throw error
+    })
+    req.write(data)
+    req.end()
+  }
+
+  addComment (string) {
+    const data = JSON.stringify({
+      body: string
+    })
+    this.httpsApiRequest(data, 'comments')
+  }
+
+  addLabels (array) {
+    const data = JSON.stringify({
+      labels: array
+    })
+    this.httpsApiRequest(data, 'labels')
+  }
+
+  addAssignees (array) {
+    const data = JSON.stringify({
+      assignees: array
+    })
+    this.httpsApiRequest(data, 'assignees')
+  }
+}
